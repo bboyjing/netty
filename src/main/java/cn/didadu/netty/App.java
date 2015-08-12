3155adec6c5535b63f8f35eb82f3eb09abfc59db
张靖 <zhangjing@maijia.com>
2015-08-12 10:31:50 +0800

    add netty maven project

---
+++ src/main/java/cn/didadu/netty/App.java
@@ -0,0 +1,13 @@
+package cn.didadu.netty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
